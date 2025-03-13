bfa6841130c63647099f74c33c50515ed0e99260
NightBlade <nguyenhakien8@gmail.com>
2025-03-13 23:40:43 +0700

    Add project files.

---
+++ ViewModels/EditItemVM.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Shopping_Cart_2.Attributes;
+using Shopping_Cart_2.Sittings;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopping_Cart_2.ViewModels
+{
+    public class EditItemVM : BaseItemVM
+    {
+
+        public int Id { get; set; }
+
+        public string? CurrentCover { get; set; }
+
+        [AllowedExtensions(FileSettings.AllowedExtensions)]
+        [MaxFileSize(FileSettings.MaxFileSizeInBytes)]
+        public IFormFile? Cover { get; set; } = default!;
+
+        
+
+    }
+}
